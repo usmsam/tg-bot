839f2f030fba522af1ce49f26a68b60e06baf287
Sam <samandarusmanov1@gmail.com>
2024-10-11 00:12:25 +0500

    remove types

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 import bodyParser from 'body-parser'
-import express, { Request, Response } from 'express'
-import TelegramBot, { Message } from 'node-telegram-bot-api'
+import express from 'express'
+import TelegramBot from 'node-telegram-bot-api'
 
 const app = express()
 
 // Используйте bodyParser для обработки JSON
 app.use(bodyParser.json())
 
-const token: string = 'YOUR_TELEGRAM_BOT_TOKEN'
+const token = '7214643228:AAFiUCOeloGJawNnDuzHxgqZ2K-eMHIvhZw'
 const bot = new TelegramBot(token)
 
 // Укажите URL для вебхука
-const webhookUrl: string = 'tg-bot-gules.vercel.app/api/bot'
+const webhookUrl = 'tg-bot-gules.vercel.app/api/bot'
 
 // Установите вебхук
 bot.setWebHook(webhookUrl).then(() => {
@@ -19,20 +19,20 @@ bot.setWebHook(webhookUrl).then(() => {
 })
 
 // Обработка входящих обновлений от Telegram
-app.post('/api/bot', (req: Request, res: Response) => {
+app.post('/api/bot', (req, res) => {
 	const update = req.body // Получите данные обновления
 	bot.processUpdate(update) // Обработайте обновление
 	res.sendStatus(200) // Верните статус 200 OK
 })
 
 // Обработка команды /start
-bot.onText(/\/start/, (msg: Message) => {
+bot.onText(/\/start/, msg => {
 	const chatId = msg.chat.id
 	bot.sendMessage(chatId, 'Добро пожаловать! Как я могу помочь?')
 })
 
 // Обработка любых других сообщений
-bot.on('message', (msg: Message) => {
+bot.on('message', msg => {
 	const chatId = msg.chat.id
 	const text = msg.text
 
